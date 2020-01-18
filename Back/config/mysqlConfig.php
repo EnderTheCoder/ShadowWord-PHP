bd9510c673e51cf7635641b4a0df91fddf6333c1
Evilrabbit520 <wangjianing@inidt.com>
2020-01-18 23:25:03 +0800

    Complete judgment, add PCPage.

--- Back/config/mysqlConfig.php
+++ Back/config/mysqlConfig.php
@@ -1,5 +1,5 @@
 <?php
-define("MYSQL_HOST", "localhost", true);
-define("MYSQL_DB_NAME", "ShadoWords", true);
+define("MYSQL_HOST", "frp.yishugou.shop", true);
+define("MYSQL_DB_NAME", "ShadowWord", true);
 define("MYSQL_USER_NAME", "Ender", true);
 define("MYSQL_PASSWORD", "Feng,HK,4778!", true);
